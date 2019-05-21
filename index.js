d97b852f001d6e1a5440f5d84af32d9cb44a3eb3
Felipe Acosta <facosta0787@gmail.com>
2019-05-21 01:07:30 -0500

    [API] Second commit

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
-const express = require('express')
+const app = require('express')()
 const helmet = require('helmet')
-const query = require('./lib/database').querySelect()
+const cors = require('cors')
+const db = require('./lib/dbconn')()
 
-const app = express()
-
-// add some security-related headers to the response
 app.use(helmet())
+app.use(cors())
 
-app.get('*', (req, res) => {
-    query(`
-      select *
-      from centroUnido_tblUsuarios
-    `)
-    .then(result => {
-      return res.status(200).json({
-        status: 200,
-        message: 'ok',
-        path: '/',
-        data: result
-      })
-    })
-    .catch(err => {
-      return res.json(err)
-    })
+app.get('/', async (req, res) => {
+  try {
 
+    const result = await db.query('select id, nombre, usuario from centroUnido_tblUsuarios',{ type: 'SELECT' })
+    return res.json(result)
+
+  } catch(err) {
+
+    return res.json(err)
+  }
 })
 
 module.exports = app
